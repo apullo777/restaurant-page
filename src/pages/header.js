f5972759c2f3e65ba46d94ea16e004c6fa375010
apullo777 <peter.yaochen.chang@gmail.com>
2022-08-08 15:13:15 +0200

    fix homepage

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,29 +1,38 @@
 import zEmblemImg from "../assets/img/zero-logo.png";
 import zeroLogo from "../assets/img/z-emblem.webp";
 import zChaserImg from "../assets/img/zchaser-logo.png";
+// import bgMusic from "../assets/music/theme-of-zero.mp3";
 
-const headerDiv = function(){
+const headerDiv = () => {
     const header = document.createElement("div");
     header.id= "header";
 
     const zEmblem = document.createElement("img");
     zEmblem.src = zEmblemImg;
     zEmblem.id = "z-emblem";
-
     header.appendChild(zEmblem);
 
     const char = document.createElement("img");
     char.src = zeroLogo;
     char.id= "char-page";
-
     header.appendChild(char);
 
     const zChaser = document.createElement("img");
     zChaser.src = zChaserImg;
     zChaser.id= "z-chaser";
-
     header.appendChild(zChaser);
-    
+
+    /* 
+    const bgm = document.createElement("audio")
+    bgm.setAttribute("controls", "")
+    bgm.autoplay = "true"
+    const source = document.createElement("source")
+    source.src = bgMusic
+    source.type = "audio/mp3"
+    bgm.appendChild(source);
+    header.appendChild(bgm);
+    */
+
     return header;
 }
 
